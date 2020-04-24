8e8ee917519a80caa83b3b835aa475908833fddf
ZTF <ZTF666@protonmail.ch>
2020-04-24 08:24:13 +0000

    :heart: Aiko :heart:

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="dark" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          :src="logo"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          :src="logotitle"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+        <span class="mr-2">Github repo</span>
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <ztf />
+    </v-content>
+    <!--  -->
+
+    <!--  -->
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ztf from "./components/ztf";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    ztf
+  },
+
+  data: () => ({
+    logo: require("../src/assets/me.png"),
+    logotitle: require("../src/assets/ztf.png")
+    //
+  })
+};
+</script>
---
+++ src/components/ztf.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container>
+    <!-- input field -->
+    <v-form @submit.prevent="getAvatarIg()">
+      <v-container>
+        <v-layout row wrap class="d-flex justify-center">
+          <v-flex xs12 sm6 md3>
+            <v-text-field label="Outline" outline v-model="username"></v-text-field>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-form>
+    <!-- cards -->
+    <v-layout class="d-flex justify-center mt-10">
+      <v-flex xs12 sm6 lg2 md4>
+        <v-card>
+          <v-img class="white--text" height="320px" width="320px" :src="avatar">
+            <!-- <v-container fill-height fluid> -->
+            <!-- <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline">{{platform}}</span>
+                </v-flex>
+            </v-layout>-->
+            <!-- </v-container> -->
+          </v-img>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ztf",
+
+  data() {
+    return {
+      avatar: "",
+
+      platform: "",
+      username: "",
+      myurl: "",
+      status: ""
+    };
+  },
+  methods: {
+    async getAvatarIg() {
+      let apiUrl = "https://unavatar.now.sh/instagram/";
+      apiUrl = apiUrl + this.username + "?json";
+      await fetch(apiUrl).then(response => (this.status = response.status));
+      console.log(this.status);
+      if (this.status == 200) {
+        this.avatar = "https://unavatar.now.sh/instagram/" + this.username;
+      }
+    }
+  }
+};
+</script>
